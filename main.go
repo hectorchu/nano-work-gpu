68c98355d000d5196b203090c8cc6f713dc9ff81
Hector Chu <hectorchu@gmail.com>
2020-12-14 12:38:03 +0000

    Add RPC server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
@@ -15,6 +20,7 @@ func main() {
 		root      rootFlag
 		target    difficultyFlag
 		benchmark = flag.Int("b", 0, "benchmark: number of iterations")
+		server    = flag.String("s", "", "server: address to listen for RPC requests")
 	)
 	target.Set("fffffff800000000")
 	flag.Var(&root, "r", "root: 32-byte hex string")
@@ -22,6 +28,14 @@ func main() {
 	flag.Parse()
 	if *benchmark > 0 {
 		runBenchmark(target.n, *benchmark)
+		return
+	}
+	if *server != "" {
+		http.HandleFunc("/", rpcHandler)
+		if err := http.ListenAndServe(*server, nil); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		return
 	}
 	if root == nil {
@@ -54,6 +68,42 @@ func runBenchmark(target uint64, n int) {
 		fmt.Printf("%d: %x %x %s\n", i, root, work, elapsed)
 	}
 	fmt.Printf("Average %s\n", total/time.Duration(n))
+}
+
+func rpcHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		buf    bytes.Buffer
+		v      struct{ Action, Difficulty, Hash string }
+		root   rootFlag
+		target difficultyFlag
+	)
+	io.Copy(&buf, r.Body)
+	r.Body.Close()
+	if err := json.Unmarshal(buf.Bytes(), &v); err != nil {
+		return
+	}
+	if v.Action != "work_generate" {
+		return
+	}
+	if err := root.Set(v.Hash); err != nil {
+		return
+	}
+	target.Set("fffffff800000000")
+	if v.Difficulty != "" {
+		if err := target.Set(v.Difficulty); err != nil {
+			return
+		}
+	}
+	work, err := pow.GenerateGPU(root, target.n)
+	if err != nil {
+		return
+	}
+	rev(work)
+	buf.Reset()
+	json.NewEncoder(&buf).Encode(map[string]string{
+		"work": hex.EncodeToString(work),
+	})
+	io.Copy(w, &buf)
 }
 
 func rev(b []byte) {
